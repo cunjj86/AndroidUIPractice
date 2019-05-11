ed74b1d637a938b8c79c5f73635b06530e2530e9
cunjj86 <ljgcunjj@naver.com>
2019-05-11 11:14:51 +0900

    프래그먼트 데이터 바인딩 이용

---
+++ app/src/main/java/com/tje/androiduipractice/fragments/FragmentOne.java
@@ -0,0 +1,35 @@
+package com.tje.androiduipractice.fragments;
+
+import android.databinding.DataBindingUtil;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.tje.androiduipractice.R;
+import com.tje.androiduipractice.databinding.Fragment1Binding;
+
+public class FragmentOne extends Fragment {
+
+    Fragment1Binding binding;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_1, container, false);
+
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        binding.myTextView.setText("데이터 바인딩 이용");
+
+    }
+}
