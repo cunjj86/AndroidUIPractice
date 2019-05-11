5bfdcfa9df79c12bf66d9257f2c3722c10bf7e1b
cunjj86 <ljgcunjj@naver.com>
2019-05-11 12:46:10 +0900

    프래그먼트가 많아질 경우 필요한 메소드 작성

--- app/src/main/java/com/tje/androiduipractice/MainActivity.java
+++ app/src/main/java/com/tje/androiduipractice/MainActivity.java
@@ -110,6 +110,8 @@ public class MainActivity extends BaseActivity {
     @Override
     public void setValues() {
 
+        act.viewPager.setOffscreenPageLimit(4); // 프래그먼트의 갯수와 맞춰주자!!
+
         PagerAdapter pagerAdapter = new PagerAdapter(getSupportFragmentManager(), 3);
         act.viewPager.setAdapter(pagerAdapter);
 
