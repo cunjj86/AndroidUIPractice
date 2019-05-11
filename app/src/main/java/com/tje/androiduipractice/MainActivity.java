0bfb0810cb0422e88b2a5926e63c4a7e2228f76a
cunjj86 <ljgcunjj@naver.com>
2019-05-11 12:02:33 +0900

    프래그먼트를 가로 스크롤로 배치 - 뷰 페이지로 교체할 예정

--- app/src/main/java/com/tje/androiduipractice/MainActivity.java
+++ app/src/main/java/com/tje/androiduipractice/MainActivity.java
@@ -1,10 +1,15 @@
 package com.tje.androiduipractice;
 
 import android.databinding.DataBindingUtil;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import com.tje.androiduipractice.databinding.ActivityMainBinding;
+import com.tje.androiduipractice.fragments.FragmentTwo;
 
 public class MainActivity extends BaseActivity {
 
@@ -21,6 +26,20 @@ public class MainActivity extends BaseActivity {
 
     @Override
     public void setupEvents() {
+
+        act.changeFragTwoBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Fragment fr = new FragmentTwo();
+
+                FragmentManager fragmentManager = getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.fragOne, fr);
+                fragmentTransaction.commit();
+
+            }
+        });
 
     }
 
